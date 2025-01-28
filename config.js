aeb3d28cb0786c3026830e01e60b20ba913bc652
osho2025 <bellodivine135@gmail.com>
2025-01-28 17:13:24 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ require("dotenv").config();
 
 module.exports = {
     prefix: process.env.PREFIX || ".",
-    ownerName: process.env.OWNER_NAME || "Toxxic-Boy",
-    ownerNumber: process.env.OWNER_NUMBER || "2348165846414",
+    ownerName: process.env.OWNER_NAME || "Darkweb",
+    ownerNumber: process.env.OWNER_NUMBER || "2348114226898",
     mode: process.env.MODE || "public",
     region: process.env.REGION || "Nigeria",
     botName: process.env.BOT_NAME || "Rias Gremory V3",
@@ -12,9 +12,9 @@ module.exports = {
     exifAuthor: process.env.EXIF_AUTHOR || "Toxxic",
     timeZone: process.env.TIME_ZONE || "Africa/Lagos",
     presenceStatus: process.env.PRESENCE_STATUS || "unavailable",
-    autoRead: process.env.AUTO_READ === "true",
+    autoRead: process.env.AUTO_READ === "false",
     autoViewStatus: process.env.AUTO_VIEW_STATUS === "true",
-    autoReact: process.env.AUTO_REACT === "true",
+    autoReact: process.env.AUTO_REACT === "false",
     sessionId: process.env.SESSION_ID || "",
     autoRejectEnabled: process.env.AUTO_REJECT_ENABLED || "false",
 };
